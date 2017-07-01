c0f6b7373732147d006185b7bb2e8ac0b1718fa8
yahya <yahyacan55@gmail.com>
2017-07-01 12:02:12 +0300

    Initial commit

---
+++ app/src/main/java/com/ismek/barcodescanner/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ismek.barcodescanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            Toast.makeText(MainActivity.this,re,Toast.LENGTH_LONG).show();
+            Log.d("code", re);
+        }
+
+    }
+}
